888f1bc6d3a7d2434b266a16f5d746140b9ab4b6
ismail simsek <ismailsimsek09@gmail.com>
2026-03-19 12:40:32 +0100

    fix import

--- src/QueryField.test.tsx
+++ src/QueryField.test.tsx
@@ -1,4 +1,5 @@
-import React, { act, render, renderHook, screen, waitFor } from '@testing-library/react';
+import React from 'react';
+import { act, render, renderHook, screen, waitFor } from '@testing-library/react';
 
 import { CascaderOption } from '@grafana/ui';
 
