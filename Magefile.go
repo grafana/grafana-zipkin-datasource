b4914ba1c00a8718edadfdff6b428fb0864b7dbe
ismail simsek <ismailsimsek09@gmail.com>
2026-04-14 13:12:44 +0200

    override plugin-sdk's BuildAll to enable extra platforms

--- Magefile.go
+++ Magefile.go
@@ -9,4 +9,24 @@ import (
 )
 
 // Default configures the default target.
-var Default = build.BuildAll
+var Default = BuildAll
+
+// BuildAll builds all default targets plus linux/s390x and windows/arm64,
+// to support bundling with Grafana builds on those platforms. This overrides
+// the buildAll from grafana-plugin-sdk-go.
+func BuildAll() {
+	b := build.Build{}
+	mg.Deps(b.Linux, b.Windows, b.Darwin, b.DarwinARM64, b.LinuxARM64, b.LinuxARM, LinuxS390X, WindowsARM64)
+}
+
+// Test() wraps the plugin SDK's Test to make it accessible since we're
+// no longer using mage:import
+func Test() error {
+	return build.Test()
+}
+
+// TestRace() wraps the plugin SDK's Test to make it accessible since we're
+// no longer using mage:import
+func TestRace() error {
+	return build.TestRace()
+}
